5e9b53c01b1a9b04e6df46f4b10a7b4b75084ac4
lakesidefood01-blip <lakesidefood01@gmail.com>
2026-05-18 10:10:20 +0700

    fix: Update gallery contact number
    Updates the contact phone number displayed in several gallery item footers from '08155044405' to '085819991589'.

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -12,167 +12,167 @@ export const galleryItems: GalleryItem[] = [
     image: 'https://bundanurcatering.com/img/gallery/26.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/26.jpg',
     title: 'Pesanan Nasi Kotak Bunda Nur',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '25',
     image: 'https://bundanurcatering.com/img/gallery/25.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/25.jpg',
     title: 'Pesanan Nasi Bento Bunda Nur',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '29',
     image: 'https://bundanurcatering.com/img/gallery/29.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/29.jpg',
     title: 'Pesanan Nasi Kotak Bunda Nur',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '4',
     image: 'https://bundanurcatering.com/img/gallery/4.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/4.jpg',
     title: 'Pesanan Paket Aqiqah',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '1',
     image: 'https://bundanurcatering.com/img/gallery/1.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/1.jpg',
     title: 'Pesanan Paket Aqiqah',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '22',
     image: 'https://bundanurcatering.com/img/gallery/22.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/22.jpg',
     title: 'Pesanan Paket Aqiqah',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '7',
     image: 'https://bundanurcatering.com/img/gallery/7.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/7.jpg',
     title: 'Pesanan Kue Bunda Nur',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '8',
     image: 'https://bundanurcatering.com/img/gallery/8.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/8.jpg',
     title: 'Pesanan Kue Bunda Nur',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '9',
     image: 'https://bundanurcatering.com/img/gallery/9.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/9.jpg',
     title: 'Pesanan Kue Bunda Nur',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '2',
     image: 'https://bundanurcatering.com/img/gallery/2.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/2.jpg',
     title: 'Pesanan Ayam Bakar Hantaran',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '11',
     image: 'https://bundanurcatering.com/img/gallery/11.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/11.jpg',
     title: 'Pesanan Nasi Tumpeng',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '5',
     image: 'https://bundanurcatering.com/img/gallery/5.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/5.jpg',
     title: 'Pesanan Nasi Tumpeng Mini',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '13',
     image: 'https://bundanurcatering.com/img/gallery/13.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/13.jpg',
     title: 'Pesanan Nasi Tumpeng Mini',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '14',
     image: 'https://bundanurcatering.com/img/gallery/14.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/14.jpg',
     title: 'Pesanan Nasi Tumpeng Mini',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '31',
     image: 'https://bundanurcatering.com/img/gallery/31.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/31.jpg',
     title: 'Pesanan Nasi Tumpeng Millenial',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '16',
     image: 'https://bundanurcatering.com/img/gallery/16.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/16.jpg',
     title: 'Pesanan Nasi Kotak',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '17',
     image: 'https://bundanurcatering.com/img/gallery/17.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/17.jpg',
     title: 'Pesanan Nasi Kotak',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '18',
     image: 'https://bundanurcatering.com/img/gallery/18.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/18.jpg',
     title: 'Pesanan Nasi Kotak',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '19',
     image: 'https://bundanurcatering.com/img/gallery/19.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/19.jpg',
     title: 'Pesanan Nasi Bakar',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '20',
     image: 'https://bundanurcatering.com/img/gallery/20.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/20.jpg',
     title: 'Pesanan Nasi Tumpeng Millenial',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '21',
     image: 'https://bundanurcatering.com/img/gallery/21.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/21.jpg',
     title: 'Pesanan Nasi Tumpeng Millenial',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '3',
     image: 'https://bundanurcatering.com/img/gallery/3.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/3.jpg',
     title: 'Pesanan Nasi Kuning Bento',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '23',
     image: 'https://bundanurcatering.com/img/gallery/23.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/23.jpg',
     title: 'Pesanan Nasi Tumpeng Mini',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
   {
     id: '24',
     image: 'https://bundanurcatering.com/img/gallery/24.jpg',
     thumb: 'https://bundanurcatering.com/img/gallery/crop/24.jpg',
     title: 'Pesanan Nasi Tumpeng',
-    footer: 'Hubungi 08155044405'
+    footer: 'Hubungi 085819991589'
   },
 ];
